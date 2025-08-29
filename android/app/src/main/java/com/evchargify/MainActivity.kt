b57fe11a3b7d99b97419f3e595b8ef24aa2e95d0
Danish-EvDock <danish@evdock.app>
2025-08-29 15:21:35 +0530

    Update project name from 'ev_dock' to 'ev_chargify' in MainActivity and settings.gradle

--- android/app/src/main/java/com/evchargify/MainActivity.kt
+++ android/app/src/main/java/com/evchargify/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ev_dock"
+  override fun getMainComponentName(): String = "ev_chargify"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
